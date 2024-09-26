98946f5f1f90b37bab1b0905fcbb85bf9cccad6a
Amit-Yadav21 <amit21navgurukul.org>
2024-09-27 04:53:11 +0530

    create router and successfully testing user Profile, update login data, delete login data

--- Controller/userController.js
+++ Controller/userController.js
@@ -8,7 +8,7 @@ dotenv.config();
 const secretKey = process.env.JWT_SECRET_KEY; 
 
 //Signup route
-const resistor = async (req, res) => {
+const register = async (req, res) => {
     const { name, email, password} = req.body;
 
     if (!name || !email || !password) {
@@ -34,7 +34,7 @@ const resistor = async (req, res) => {
         res.status(201).json({
             message: 'User created successfully',
             user: {
-                id: newUser._id,
+                name: newUser.name,
                 email: newUser.email,
                 role: newUser.role
             }
@@ -46,7 +46,7 @@ const resistor = async (req, res) => {
 };
 
 // Login route
-const Login = async (req, res) => {
+const login = async (req, res) => {
     const { email, password } = req.body;
   
     if (!email || !password) {
@@ -76,19 +76,6 @@ const Login = async (req, res) => {
   }
 };
 
-// Logout route
-const logout = (req, res, next) => {
-    try {
-        // Clear the token cookie on the client side
-        res.clearCookie('token');
-
-        // Send a response indicating successful logout
-        res.status(200).json({ message: 'Logout successful' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error logging in', error: err.message });
-    }
-};
-
 //get user by id route
 const getUserById = async (req, res) => {
     try {
@@ -102,16 +89,99 @@ const getUserById = async (req, res) => {
     }
 };
 
-
 //get all users
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.find();
         res.status(200).json(users)
     } catch (err) {
-        console.log(err.message);
         res.status(500).json({ message: 'Error fetching users', error: err.message });
     }
 };
 
-export {resistor, Login, logout, getUserById, getAllUsers};
+// User Profile
+const userProfile = async (req, res) => {
+    try {
+        if (!req.user) {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const profile = await User.findOne({ email: req.user.email });
+        if (!profile) {
+            res.status(404).json({ message: 'User not logedIn found' });
+        }
+        res.json({
+            UserProfile: {
+                Name: profile.name,
+                Email: profile.email,
+                Role: profile.role
+            }
+        });        
+    } catch (error) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// update function
+const updateLoginData = async (req, res) => {
+    const { name, password } = req.body; 
+    try {
+        const userEmail = req.user.email;
+        const updatedItem = await User.findOne({ email: userEmail });
+
+        if (!updatedItem) {
+            return res.status(404).json({ message: 'User not found. Please check information.' });
+        }
+
+        const updates = {};
+
+        if (name) {
+            updates.name = name; 
+        }
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            updates.password = await bcrypt.hash(password, salt); 
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            { email: userEmail },
+            { $set: updates },
+            { new: true }
+        );
+
+        res.status(200).json({ message: "User updated successfully", updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating user', error: error.message });
+    }
+};
+
+// Delete function
+const deleteUserData = async (req, res) => {
+    try {
+        const deletedItem = await User.findOneAndDelete({ email:req.user.email });
+        if (!deletedItem) {
+            res.status(404).json({ message: 'User data not found.' });
+        }
+
+        res.json({ message: 'User data deleted successfully...' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching users', error: err.message });
+    }
+};
+
+// Logout route
+const logout = (req, res) => {
+    try {
+        // Clear the token cookie on the client side
+        res.clearCookie('token');
+
+        // Send a response indicating successful logout
+        res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error: err.message });
+    }
+};
+
+
+export {register, login, getUserById, getAllUsers, userProfile, updateLoginData, deleteUserData, logout};
