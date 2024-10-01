535dd38fe9e4a9eaaf22cb1ddbc59b926f6af4ea
Amit-Yadav21 <amit21navgurukul.org>
2024-10-01 11:03:45 +0530

    create find project router  by ProjectrName, add project validation and ErrorHandler

--- Router/projectRouter.js
+++ Router/projectRouter.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import { createProject, findAllproject, findSingleProject, updateProject, projectStatus, deleteProject } from '../Controllers/projectController.js';
+import { createProject, findAllproject, findSingleProject, findLoginUserProjects, updateProject, projectStatus, deleteProject } from '../Controllers/projectController.js';
 import { verifyToken } from '../Middleware/authentication.js';
+import { validatecreateProject, validateupdateProject, validatechangeProjectStatus } from '../Validators/projectValidation.js';
+import handleValidationErrors from '../Middleware/handleValidationErrors.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createProject);
-router.get('/allProjects', verifyToken, findAllproject);
-router.get('/findProject/:id', verifyToken, findSingleProject);
-router.put('/updateProject/:id', verifyToken, updateProject);
-router.patch('/updateStatus', verifyToken, projectStatus); 
-router.delete('/delete', verifyToken, deleteProject); 
+router.post('/create/project', verifyToken, validatecreateProject, handleValidationErrors, createProject);
+router.get('/find/all/Projects', verifyToken, findAllproject);
+router.get('/find/project', verifyToken, findLoginUserProjects);
+router.get('/find/single/project', verifyToken, findSingleProject);
+router.put('/update/project', verifyToken, validateupdateProject, handleValidationErrors, updateProject);
+router.patch('/change/project/status', verifyToken, validatechangeProjectStatus, handleValidationErrors, projectStatus); 
+router.delete('/delete/project', verifyToken, deleteProject); 
 
 export default router;
