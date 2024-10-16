37433d89b3059299c8e37868d9a719b537614cd3
Amit-Yadav21 <amit21navgurukul.org>
2024-10-16 09:32:24 +0530

    update task status by login employee

--- Router/employeeTaskBoardRouter.js
+++ Router/employeeTaskBoardRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { employeeTaskDashboard } from '../Controllers/employeeTaskDashboardController.js';
+import { employeeTaskDashboard, updateTaskStatus } from '../Controllers/employeeTaskDashboardController.js';
 import { verifyToken } from '../Middleware/authentication.js';
 import handleValidationErrors from '../Middleware/handleValidationErrors.js';
 
 const router = express.Router();
 
 router.get('/employeeTaskDashboard', handleValidationErrors, verifyToken, employeeTaskDashboard);
+router.put('/update/taskstatus/:taskID', handleValidationErrors, verifyToken, updateTaskStatus);
 
 export default router;
